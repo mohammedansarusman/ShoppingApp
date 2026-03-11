6ce67b45daa95834a110ab6b790ca5979100a166
ansar <mohammedansarusman@gmail.com>
2026-03-12 01:12:54 +0400

    shimmer added in search items and local storage implemented for previous items search

--- app/components/search-items/SearchBox.tsx
+++ app/components/search-items/SearchBox.tsx
@@ -5,9 +5,10 @@ import { useState, useEffect } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useSearchItems } from "@/app/hooks/useSearchItems";
 import Image from "next/image";
+import { SearchShimmer } from "./SearchShimmer";
 
 export const SearchBox = () => {
-  const [focus, setFocus] = useState(false);  // to track focus state of the search input
+  const [focus, setFocus] = useState(false); // to track focus state of the search input
   const [searchTerm, setSearchTerm] = useState(""); // to track the current value of the search input
 
   const router = useRouter();
@@ -15,47 +16,56 @@ export const SearchBox = () => {
   const pathname = usePathname();
 
   const query: string | null = searchParams.get("query");
-  
+
   // console.log("Query from URL: ", query);
   // Fetch search results based on the query parameter
-  const {products, isLoading, error} = useSearchItems(query);
+  const { products, isLoading, error } = useSearchItems(query);
   console.log("products in searchBox: ", products);
 
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    const params = new URLSearchParams(searchParams);
-    params.set("query", searchTerm);
-    router.push(`${pathname}/?${params.toString()}`);
+    let searches: string[] = [];
+    const existing = localStorage.getItem("search");
+    if (existing) {
+      const flag = JSON.parse(existing).some(
+        (item: string) => item === searchTerm,
+      );
+      if (flag) {
+        return;
+      } else {
+        searches = [...JSON.parse(existing), searchTerm];
+      }
+    } else {
+      searches.push(searchTerm);
+    }
+    localStorage.setItem("search", JSON.stringify(searches));
   };
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setSearchTerm(e.target.value);
   };
   useEffect(() => {
-    console.log("useEffect triggered");
     const timeOut = setTimeout(() => {
       const params = new URLSearchParams(searchParams);
       params.set("query", searchTerm);
-      router.replace(`${pathname}/?${params.toString()}`);      
-
-    },500)
-    return ()=>{
+      router.replace(`${pathname}/?${params.toString()}`);
+    }, 500);
+    return () => {
       clearTimeout(timeOut);
-    }
+    };
   }, [searchTerm]);
   useEffect(() => {
-  if (query) {
-    setSearchTerm(query);
-  }else{
-    router.push(`${pathname}`);
-
-  }
-}, [query]);
+    if (query) {
+      setSearchTerm(query);
+    } else {
+      router.push(`${pathname}`);
+    }
+  }, [query]);
 
   return (
     <>
       <form
         className="w-full h-12 flex justify-center rounded-md md:hidden"
-        onSubmit={handleSearch}
+        onSubmit={handleSubmit}
       >
         <div
           className={`w-3/4 h-full border-l border-t border-b ${focus ? "border-pink-500" : "border-gray-300"} 
@@ -78,17 +88,18 @@ export const SearchBox = () => {
           <SearchIcon color="white" />
         </button>
       </form>
-      {isLoading && <p className="mt-4 text-center">Loading...</p>}
+      {isLoading && <SearchShimmer />}
       {error && <p className="mt-4 text-center">Error: {String(error)}</p>}
-      {products && (
+      {products.length > 0 && (
         <div className="w-full px-4 ">
           <h1 className="mt-4">Suggested Products</h1>
           {products.map((item) => (
-            <div  key={item.id} className="w-full flex flex-col justify-center gap-4 mt-2 bg-gray-100 
-            rounded-md cursor-pointer py-2 hover:bg-gray-200 transition-colors duration-300">
-              <div
-                className="w-full h-20 flex justify-start items-center gap-3"
-              >
+            <div
+              key={item.id}
+              className="w-full flex flex-col justify-center gap-4 mt-2 bg-gray-100 
+              rounded-md cursor-pointer py-2 hover:bg-gray-200 transition-colors duration-300"
+            >
+              <div className="w-full h-20 flex justify-start items-center gap-3">
                 <div className="w-20 h-full">
                   <Image
                     src={item.thumbnail}
---
+++ app/components/search-items/SearchShimmer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const SearchShimmer = () => {
+  return (
+    <div className="w-full h-screen px-4">
+        <div className="w-full h-20 flex justify-start items-center gap-3 mt-4 rounded-md bg-gray-200"></div>
+        <div className="w-full h-20 flex justify-start items-center gap-3 mt-4 rounded-md bg-gray-200"></div>
+        <div className="w-full h-20 flex justify-start items-center gap-3 mt-4 rounded-md bg-gray-200"></div>
+        <div className="w-full h-20 flex justify-start items-center gap-3 mt-4 rounded-md bg-gray-200"></div>
+        <div className="w-full h-20 flex justify-start items-center gap-3 mt-4 rounded-md bg-gray-200"></div>
+        <div className="w-full h-20 flex justify-start items-center gap-3 mt-4 rounded-md bg-gray-200"></div>
+    </div>
+  );
+};
